6de5fc30017fd26521ce24265f969500955de31d
shelynna <sheilamakynhills@gmail.com>
2022-10-13 19:10:27 -0700

    Starting C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- This is a special function that serve as the entry point program
+ *
+ * Return: It returns the execution of a funtion by returning zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multingual puzzle");
+	return (0);
+}
