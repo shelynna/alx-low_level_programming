65df6519a7835ab6d2154627689b2dc86c2e06b7
shelynna <sheilamakynhills@gmail.com>
2022-10-18 21:31:06 -0700

    task 3

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,5 @@
 #ifndef main_h
 #define main_h
-extern int _putchar( char c);
-extern void print_alphabet(void);
-extern void print_alphabet_x10(void);
-extern int_islower(int c);
-extern int_isalpha(int c);
-extern int print_sign(int n);
-extern int_abs (int);
-extern int print_last_digit(int);
-extern void jack_bauer(void);
-extern void times_table(void); 
-extern int add(int, int);
-extern void print_times_table(int n); 
-#endif
+int _putchar( char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
