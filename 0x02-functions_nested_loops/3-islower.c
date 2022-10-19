8e6d0766b3cda1be56030f9456315bd3ff118a87
shelynna <sheilamakynhills@gmail.com>
2022-10-18 21:27:54 -0700

    islowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,14 +5,14 @@
  * Return: 0-if lowercase, 1-if not.
  */
 
-int _islower( int c )
+int _islower(int c)
 {
-	if ( c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
