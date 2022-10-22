3e93c8c90f761eede69b36e12e42563fabdf34c4
shelynna <sheilamakynhills@gmail.com>
2022-10-21 18:55:50 -0700

    all advance or optional task

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
-*print_most_numbers - prints
-* 
+*print_most_numbers - prints values
 *Return: void
 */
 
@@ -12,7 +11,8 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c);
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
 	}
 	_putchar('\n');
 }
