2c077d106c5b7d22fbde051dd302bd07e7fe7b20
shelynna <sheilamakynhills@gmail.com>
2022-10-25 11:26:09 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,14 +4,11 @@
  *@s:String pointer to the string whose length is to be found.
  *Return: returns the length of the string.
  */
-
 int _strlen(char *s)
 {
-	int p = 0;
-	/ incremeant up to when the last character is NULL, \ 0 /
-	while (*(s + p) != 0)
-	{
-		p++;
-	}
-	return (p);
+	int a = 0;
+
+	for (; *s++;)
+		a++;
+	return (a);
 }
