f4a4ff21bcbb46850842d2efda4fb0682fb95dea
shelynna <sheilamakynhills@gmail.com>
2022-11-09 20:46:55 -0800

    update on static library

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_strlen-Finds the length of a string.
+ *@s:String pointer to the string whose length is to be found.
+ *Return: returns the length of the string.
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s++;)
+		a++;
+	return (a);
+}
